7d2f66410806f2de8236673602cdbfe45e72ce48
Romain Dardour <romain@hull.io>
2015-02-10 12:52:16 +0100

    Auto Grow frame on content change
    Signed-off-by: Romain Dardour <romain@hull.io>

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var Frame = React.createClass({
   },
   componentDidMount: function() {
     this.renderFrameContents();
+    // Ensure the frame doesnt scroll if it grows.
+    this._autogrow = setInterval(this.autoGrow, 200);
   },
   renderFrameContents: function() {
     var doc = this.getDOMNode().contentDocument;
@@ -21,6 +23,7 @@ var Frame = React.createClass({
       );
 
       React.render(contents, doc.body);
+      React.render(contents, doc.body, this.autoGrow);
     } else {
       setTimeout(this.renderFrameContents, 0);
     }
@@ -30,7 +33,24 @@ var Frame = React.createClass({
   },
   componentWillUnmount: function() {
     React.unmountComponentAtNode(this.getDOMNode().contentDocument.body);
-  }
+  },
+  autoGrow: function() {
+    // Automatically update height to prevent scrollbars
+    if (this.isMounted()) {
+      var frame = this.getDOMNode();
+      var doc = frame.contentDocument;
+      setTimeout(function() {
+        doc.body.style.margin = 0;
+        doc.body.style.padding = 0;
+        // Give it a bit of padding, to handle browser differences.
+        var height = doc.body.firstChild.offsetHeight + 30;
+        frame.style.height = height + "px";
+      }, 10);
+    } else {
+      // We're not mounted. Stop polling
+      if (this._autogrow) clearInterval(this._autogrow);
+    }
+  },
 });
 
 
